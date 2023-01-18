59a62fa9c7a24e84299d4b92b20c3e8a653f89c9
Volkov Vadim <vadvva@yandex.ru>
2023-01-18 21:41:25 +0300

    fixed bugs HW_3 ver_3 in lesson_3

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -27,13 +27,4 @@ public class SortedArrayStorage extends AbstractArrayStorage {
         searchKey.setUuid(uuid);
         return Arrays.binarySearch(storage, 0, countResume, searchKey);
     }
-
-    public void sort(Resume[] arrayResumes) {
-        for (int i = 0; i < countResume; i++) {
-            Resume current = arrayResumes[i];
-            int j = Math.abs(Arrays.binarySearch(arrayResumes, 0, i, current) + 1);
-            System.arraycopy(arrayResumes, j, arrayResumes, j + 1, i - j);
-            arrayResumes[j] = current;
-        }
-    }
 }
